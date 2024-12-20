df16ca721eb561fa9143ec1c2861114b5a65bb33
Adeeb Tyagi <117512237+adeeb4@users.noreply.github.com>
2024-12-20 22:35:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: "#39DB4A",
+        red: "#FF6868",
+        secondary: "#555",
+        prigmayBG: "#FCFCFC",
+      },
+    },
+  },
+  plugins: [daisyui],
+};
